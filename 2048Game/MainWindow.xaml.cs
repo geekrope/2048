e09edc9ec959b9e9beffef981ad942935f77af23
geekrope <geekrope@gmail.com>
2021-03-22 22:43:11 +0300

    to fix animation

--- 2048Game/MainWindow.xaml.cs
+++ 2048Game/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace _2048Game
 {
@@ -45,11 +46,11 @@ namespace _2048Game
 
         public SolidColorBrush GetColor(uint value)
         {
-            if(value == 0)
+            if (value == 0)
             {
                 return ForeColor;
             }
-            else if(value==2)
+            else if (value == 2)
             {
                 return _2;
             }
@@ -95,6 +96,11 @@ namespace _2048Game
             }
         }
 
+        public enum Direction
+        {
+            Left, Right, Up, Down
+        }
+
         class Cell
         {
             public Cell(uint value, int x, int y)
@@ -130,164 +136,178 @@ namespace _2048Game
 
         Cell[,] Cells = new Cell[CellsCount, CellsCount];
 
-        public void MoveLeft(int x, int y)
-        {            
-            var cell = Cells[x, y];
-            if (x == 0)
-            {
-                return;
-            }
-            if (Cells[x - 1, y].Value == Cells[x, y].Value)
-            {
-                var value = Cells[x, y].Value;
-                Cells[x, y].Value = 0;
-                Cells[x - 1, y].Value = value * 2;               
-            }
-            else
-            {
-                for (; x > 0;)
-                {
-                    if (Cells[x - 1, y].Value == 0)
-                    {
-                        var value = Cells[x, y].Value;
-                        Cells[x, y].Value = 0;
-                        Cells[x - 1, y].Value = value;
-                        x--;                        
-                    }
-                    else
-                    {
-                        if (Cells[x - 1, y].Value == Cells[x, y].Value)
-                        {
-                            var value = Cells[x, y].Value;
-                            Cells[x, y].Value = 0;
-                            Cells[x - 1, y].Value = value * 2;                            
-                        }
-                        break;
-                    }
-                }
-            }           
-        }
+        bool Moved = false;
 
-        public void MoveRight(int x, int y)
-        {            
-            var cell = Cells[x, y];
-            if (x == CellsCount - 1)
-            {
-                return;
-            }
-            if (Cells[x + 1, y].Value == Cells[x, y].Value)
-            {
-                var value = Cells[x, y].Value;
-                Cells[x, y].Value = 0;
-                Cells[x + 1, y].Value = value * 2;               
-            }
-            else
-            {
-                for (; x < CellsCount - 1;)
-                {
-                    if (Cells[x + 1, y].Value == 0)
-                    {
-                        var value = Cells[x, y].Value;
-                        Cells[x, y].Value = 0;
-                        Cells[x + 1, y].Value = value;
-                        x++;                       
-                    }
-                    else
-                    {
-                        if (Cells[x + 1, y].Value == Cells[x, y].Value)
-                        {
-                            var value = Cells[x, y].Value;
-                            Cells[x, y].Value = 0;
-                            Cells[x + 1, y].Value = value * 2;                            
-                        }
-                        break;
-                    }
-                }
-            }            
-        }
-
-        public void MoveUp(int x, int y)
-        {           
-            var cell = Cells[x, y];
-            if (y == 0)
-            {
-                return;
-            }
-            if (Cells[x, y - 1].Value == Cells[x, y].Value)
-            {
-                var value = Cells[x, y].Value;
-                Cells[x, y].Value = 0;
-                Cells[x, y - 1].Value = value * 2;               
-            }
-            else
-            {
-                for (; y > 0;)
-                {
-                    if (Cells[x, y - 1].Value == 0)
-                    {
-                        var value = Cells[x, y].Value;
-                        Cells[x, y].Value = 0;
-                        Cells[x, y - 1].Value = value;
-                        y--;                        
-                    }
-                    else
-                    {
-                        if (Cells[x, y - 1].Value == Cells[x, y].Value)
-                        {
-                            var value = Cells[x, y].Value;
-                            Cells[x, y].Value = 0;
-                            Cells[x, y - 1].Value = value * 2;                            
-                        }
-                        break;
-                    }
-                }
-            }            
-        }
-
-        public void MoveDown(int x, int y)
+        public void Move(int x, int y, Direction dir)
         {
-            bool moved = false;
-            var cell = Cells[x, y];
-            if (y == CellsCount - 1)
+            Point StartPos = new Point(x, y);
+            Point EndPos = new Point(x, y);
+            var val = Cells[x, y].Value;
+            int xOffset = 0;
+            int yOffset = 0;
+            if (dir == Direction.Left)
             {
-                return;
+                xOffset = -1;
+                if (x == 0)
+                {
+                    return;
+                }
             }
-            if (Cells[x, y + 1].Value == Cells[x, y].Value)
+            else if (dir == Direction.Right)
+            {
+                xOffset = 1;
+                if (x == CellsCount - 1)
+                {
+                    return;
+                }
+            }
+            else if (dir == Direction.Up)
+            {
+                yOffset = -1;
+                if (y == 0)
+                {
+                    return;
+                }
+            }
+            else if (dir == Direction.Down)
+            {
+                yOffset = 1;
+                if (y == CellsCount - 1)
+                {
+                    return;
+                }
+            }
+            if (Cells[x + xOffset, y + yOffset].Value == Cells[x, y].Value)
             {
                 var value = Cells[x, y].Value;
                 Cells[x, y].Value = 0;
-                Cells[x, y + 1].Value = value * 2;
-                moved = true;
+                Cells[x + xOffset, y + yOffset].Value = value * 2;
+                EndPos.X = x + xOffset;
+                EndPos.Y = y + yOffset;
+                Moved = true;
             }
             else
             {
-                for (; y < CellsCount - 1;)
+                bool end = false;
+                for (; !end;)
                 {
-                    if (Cells[x, y + 1].Value == 0)
+                    if (Cells[x + xOffset, y + yOffset].Value == 0)
                     {
                         var value = Cells[x, y].Value;
                         Cells[x, y].Value = 0;
-                        Cells[x, y + 1].Value = value;
-                        y++;
-                        moved = true;
+                        Cells[x + xOffset, y + yOffset].Value = value;
+                        EndPos.X = x + xOffset;
+                        EndPos.Y = y + yOffset;
+                        x += xOffset;
+                        y += yOffset;                       
+                        Moved = true;
                     }
                     else
                     {
-                        if (Cells[x, y + 1].Value == Cells[x, y].Value)
+                        if (Cells[x + xOffset, y + yOffset].Value == Cells[x, y].Value)
                         {
                             var value = Cells[x, y].Value;
                             Cells[x, y].Value = 0;
-                            Cells[x, y + 1].Value = value * 2;
-                            moved = true;
+                            Cells[x + xOffset, y + yOffset].Value = value * 2;
+                            EndPos.X = x + xOffset;
+                            EndPos.Y = y + yOffset;
+                            Moved = true;
                         }
                         break;
                     }
+                    if (dir == Direction.Left && x <= 0)
+                    {
+                        end = true;
+                    }
+                    else if (dir == Direction.Right && x >= CellsCount - 1)
+                    {
+                        end = true;
+                    }
+                    else if (dir == Direction.Up && y <= 0)
+                    {
+                        end = true;
+                    }
+                    else if (dir == Direction.Down && y >= CellsCount - 1)
+                    {
+                        end = true;
+                    }
                 }
             }
-            if (moved)
+            var timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+
+            var width = CanvasSize.Width - 2 * BlockMargin;
+            var height = CanvasSize.Height - 2 * BlockMargin;
+
+            var cellWidth = width / CellsCount - (CellsCount - 1.0) / CellsCount * BlockMargin;
+            var cellHeight = height / CellsCount - (CellsCount - 1.0) / CellsCount * BlockMargin;
+
+            var x1 = StartPos.X * cellWidth + BlockMargin * (StartPos.X + 1);
+            var y1 = StartPos.Y * cellWidth + BlockMargin * (StartPos.Y + 1);
+
+            var x2 = EndPos.X * cellWidth + BlockMargin * (EndPos.X + 1);
+            var y2 = EndPos.Y * cellWidth + BlockMargin * (EndPos.Y + 1);
+
+            var itteration = 0;
+            var itterations = 10;
+
+            var cellFill = new Rectangle();
+
+            cellFill.Fill = Cells[(int)StartPos.X, (int)StartPos.Y].Fill.Fill;
+
+            cellFill.Width = cellWidth;
+            cellFill.Height = cellHeight;
+
+            cellFill.SetValue(LeftProperty, x1 * cellWidth + BlockMargin * (x + 1));
+            cellFill.SetValue(TopProperty, y1 * cellHeight + BlockMargin * (y + 1));
+
+            cellFill.RadiusX = BorderRadius;
+            cellFill.RadiusY = BorderRadius;                       
+
+            var label = new Label();
+            label.Content = val.ToString();
+
+            var viewBox = new Viewbox();
+
+            label.Foreground = ((Label)Cells[(int)StartPos.X, (int)StartPos.Y].Content.Child).Foreground;
+
+            viewBox.Child = label;
+
+            viewBox.Width = cellWidth;
+            viewBox.Height = cellHeight;
+
+            viewBox.SetValue(LeftProperty, x1 * cellWidth + BlockMargin * (x + 1));
+            viewBox.SetValue(TopProperty, y1 * cellHeight + BlockMargin * (y + 1));
+
+            timer.Tick += (object sender, EventArgs e) =>
             {
-                CreateRandomCell();
-            }
+                var newX = (x2 - x1) * itteration / (double)itterations + x1;
+                var newY = (y2 - y1) * itteration / (double)itterations + y1;
+
+                cellFill.SetValue(LeftProperty, newX);
+                cellFill.SetValue(TopProperty, newY);
+                viewBox.SetValue(LeftProperty, newX);
+                viewBox.SetValue(TopProperty, newY);
+
+                if (itteration >= itterations)
+                {
+                    Playground.Children.Remove(cellFill);
+                    Playground.Children.Remove(viewBox);
+                    timer.Stop();
+                    UpdateGrid();
+                }
+
+                itteration++;
+            };            
+
+            if(Cells[(int)EndPos.X, (int)EndPos.Y].Value!=0)
+            {
+                Playground.Children.Add(cellFill);
+                Playground.Children.Add(viewBox);
+                timer.Start();
+                Cells[(int)StartPos.X, (int)StartPos.Y].Fill.Fill = ForeColor;
+                ((Label)Cells[(int)StartPos.X, (int)StartPos.Y].Content.Child).Content = "";
+            }            
         }
 
         public void CreateGrid()
@@ -327,6 +347,19 @@ namespace _2048Game
                     cell.RadiusX = BorderRadius;
                     cell.RadiusY = BorderRadius;
 
+                    var cellFill = new Rectangle();
+
+                    cellFill.Fill = ForeColor;
+
+                    cellFill.Width = cellWidth;
+                    cellFill.Height = cellHeight;
+
+                    cellFill.SetValue(LeftProperty, x * cellWidth + BlockMargin * (x + 1));
+                    cellFill.SetValue(TopProperty, y * cellHeight + BlockMargin * (y + 1));
+
+                    cellFill.RadiusX = BorderRadius;
+                    cellFill.RadiusY = BorderRadius;
+
                     var label = new Label();
                     label.Content = "";
 
@@ -354,15 +387,15 @@ namespace _2048Game
         {
             var rand = new Random();
             uint value = 2;
-            if(rand.NextDouble()>0.9)
+            if (rand.NextDouble() > 0.9)
             {
                 value = 4;
             }
             var index = new Random();
             var emptyCells = new List<Cell>();
-            foreach(var cell in Cells)
+            foreach (var cell in Cells)
             {
-                if(cell.Value == 0)
+                if (cell.Value == 0)
                 {
                     emptyCells.Add(cell);
                 }
@@ -378,7 +411,7 @@ namespace _2048Game
                 for (int y = 0; y < CellsCount; y++)
                 {
                     var cell = Cells[x, y];
-                    cell.Fill.Fill = GetColor(cell.Value);                   
+                    cell.Fill.Fill = GetColor(cell.Value);
                     if (cell.Value != 0)
                     {
                         ((Label)cell.Content.Child).Content = cell.Value.ToString();
@@ -406,10 +439,9 @@ namespace _2048Game
             Playground.Height = CanvasSize.Height;
 
             CreateGrid();
-
-            Cells[2, 2].Value = 16;
-            Cells[0, 2].Value = 2;
-            Cells[3, 2].Value = 16;
+           
+            Cells[0, 2].Value = 2;      
+            
             UpdateGrid();
         }
 
@@ -422,7 +454,7 @@ namespace _2048Game
                     for (int y = 0; y < CellsCount; y++)
                     {
                         var cell = Cells[x, y];
-                        MoveRight((int)cell.Position.X, (int)cell.Position.Y);
+                        Move((int)cell.Position.X, (int)cell.Position.Y, Direction.Right);
                     }
                 }
             }
@@ -433,7 +465,7 @@ namespace _2048Game
                     for (int y = 0; y < CellsCount; y++)
                     {
                         var cell = Cells[x, y];
-                        MoveLeft((int)cell.Position.X, (int)cell.Position.Y);
+                        Move((int)cell.Position.X, (int)cell.Position.Y, Direction.Left);
                     }
                 }
             }
@@ -444,7 +476,7 @@ namespace _2048Game
                     for (int y = 0; y < CellsCount; y++)
                     {
                         var cell = Cells[x, y];
-                        MoveUp((int)cell.Position.X, (int)cell.Position.Y);
+                        Move((int)cell.Position.X, (int)cell.Position.Y, Direction.Up);
                     }
                 }
             }
@@ -452,14 +484,18 @@ namespace _2048Game
             {
                 for (int x = 0; x < CellsCount; x++)
                 {
-                    for (int y = CellsCount-1; y >=0; y--)
+                    for (int y = CellsCount - 1; y >= 0; y--)
                     {
                         var cell = Cells[x, y];
-                        MoveDown((int)cell.Position.X, (int)cell.Position.Y);
+                        Move((int)cell.Position.X, (int)cell.Position.Y, Direction.Down);
                     }
                 }
             }
-            UpdateGrid();
+            if (Moved)
+            {
+                CreateRandomCell();
+                Moved = false;
+            }
         }
     }
 }
